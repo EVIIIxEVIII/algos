69419dfdaa91d06b52fc997891b6a0683ae4242e
Kirill Katz <xnlyre@protonmail.com>
2025-08-17 15:55:35 +0300

    Another one

---
+++ 139.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <string>
+#include <unordered_set>
+
+class Solution {
+public:
+    bool wordBreak(std::string s, std::vector<std::string>& wordDict) {
+        std::unordered_set<std::string> set(wordDict.begin(), wordDict.end());
+        std::vector<bool> dp(s.size() + 1, false);
+
+        dp[0] = true;
+
+        for (int i = 1; i < s.size(); ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (dp[j] && set.count(s.substr(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[s.size()];
+    }
+};
+
+
+int main() {
+    std::string str = "leetcode";
+    std::vector<std::string> wordDict{"leet","code"};
+
+    Solution solution;
+    bool val = solution.wordBreak(str, wordDict);
+
+    printf("Res: %d\n", val);
+}
