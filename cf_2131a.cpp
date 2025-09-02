68f92f7238b6b199e6c1aa1ed2193d9dd6d792d5
Kirill Katz <xnlyre@protonmail.com>
2025-09-02 22:53:37 +0300

    Another one

---
+++ cf_2131a.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int tt; cin >> tt;
+    while(tt--) {
+        int n; cin >> n;
+
+        vector<int> a(n);
+        vector<int> b(n);
+
+        for (int i = 0; i < n; ++i) { cin >> a[i];  }
+        for (int i = 0; i < n; ++i) { cin >> b[i];  }
+        int ans = 1;
+
+        for (int i = 0; i < n; ++i) {
+            ans += max(a[i] - b[i], 0);
+        }
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
+
+// (8 + 4 + 7) - (4 + 4)
