8c28174941b52b40ea8aa2243a34aa687032f79a
Kirill Katz <xnlyre@protonmail.com>
2025-09-07 18:40:24 +0300

    Another one

--- cf_1044b.cpp
+++ cf_1044b.cpp
@@ -5,7 +5,19 @@ using namespace std;
 int main() {
     int tt; cin >> tt;
     while(tt--) {
+        int n; cin >> n;
 
+        vector<long long> a(n);
+        for (int i = 0; i < n; ++i) cin >> a[i];
+        sort(a.begin(), a.end());
+
+        long long ans = 0;
+        for (int i = a.size() - 1; i >= 0; i-=2) {
+            ans += a[i];
+        }
+
+        cout << ans << '\n';
     }
 
+    return 0;
 }
