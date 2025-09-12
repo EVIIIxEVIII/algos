98446a809757dc9e230df1e139ff176051acf2c5
Kirill Katz <xnlyre@protonmail.com>
2025-09-12 23:00:39 +0300

    Try to solve 2040c

---
+++ cf_2140b.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int tt; cin >> tt;
+
+    while (tt--) {
+        int x; cin >> x;
+        cout << 2*x << '\n';
+    }
+
+    return 0;
+}
