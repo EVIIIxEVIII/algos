36392f57044a680154c19c448a6c5b22579e33f2
Kirill Katz <xnlyre@protonmail.com>
2025-09-21 12:37:02 +0300

    Add DSU

---
+++ common_algos/dsu.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int find(vector<int>& parent, int x) {
+    if (parent[x] != x) {
+        parent[x] = find(parent, parent[x]);
+    }
+
+    return parent[x];
+}
+
+void unite(vector<int>& parent, vector<int>& size, int a, int b) {
+    a = find(parent, a);
+    b = find(parent, b);
+
+    if (a == b) return;
+    if (size[a] < size[b]) swap(a, b);
+
+    parent[b] = a;
+    size[a] += size[b];
+}
+
+int main() {
+    int n; cin >> n;
+    vector<int> parent(n);
+    vector<int> size(n);
+
+
+
+}
