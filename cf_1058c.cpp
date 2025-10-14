a2398196a93e0be475929ffc27111781026e2a73
Kirill Katz <xnlyre@protonmail.com>
2025-10-14 18:52:44 +0300

    Try to solve a C

--- cf_1058c.cpp
+++ cf_1058c.cpp
@@ -7,7 +7,28 @@ int main() {
 
     int t; cin >> t;
     while (t--) {
+        int n; cin >> n;
 
+        if (n == 0) {
+            cout << "YES" << '\n';
+            continue;
+        }
+
+        int digits = 32 - __builtin_clz(n);
+        digits += digits%2;
+
+        int side1 = 0;
+        int side2 = 0;
+
+        for (int i = 0; i < digits/2; ++i) {
+            int cmp1 = ((n >> (digits - i - 1)) & 1);
+            int cmp2 = ((n >> i) & 1);
+
+            side1 += cmp1;
+            side2 += cmp2;
+        }
+
+        cout << (side1 == side2 ? "YES" : "NO") << '\n';
     }
     return 0;
 }
