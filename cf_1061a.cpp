fcc02c6e82cd64f69169759f1f4341f6aa51abcc
Kirill Katz <xnlyre@protonmail.com>
2025-10-26 22:05:25 +0200

    Solve

---
+++ cf_1061a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        cout << (n%2 == 0 ? ((n-2) / 2) : (n/2))  << '\n';
+    }
+
+    return 0;
+}
