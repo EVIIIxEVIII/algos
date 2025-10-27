31a41a3b64e43d5d5531b0c17f3ff210a1d75755
Kirill Katz <xnlyre@protonmail.com>
2025-10-27 22:15:32 +0200

    Solve a B

--- cf_1061b.cpp
+++ cf_1061b.cpp
@@ -7,7 +7,48 @@ int main() {
 
     int t; cin >> t;
     while (t--) {
+        int n, q; cin >> n >> q;
 
+        string s;
+        cin >> s;
+
+        vector<int> a(q);
+        for (int i = 0; i < q; ++i) {
+            cin >> a[i];
+        }
+
+        bool bPresent = false;
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == 'B') {
+                bPresent = true;
+                break;
+            }
+        }
+
+        if (!bPresent) {
+            for (int i = 0; i < q; ++i) {
+                cout << a[i] << '\n';
+            }
+            continue;
+        }
+
+        for (int i = 0; i < q; ++i) {
+            int val = a[i];
+
+            int ans = 0;
+            int j = 0;
+            while (val) {
+                if (s[j] == 'A') {
+                    val--;
+                } else {
+                    val /= 2;
+                }
+                j = (j+1)%n;
+                ans++;
+            }
+
+            cout << ans << '\n';
+        }
     }
     return 0;
 }
