d009feea24defdaab61e359be05b25ffedb1b52f
Kirill Katz <xnlyre@protonmail.com>
2025-11-09 13:22:43 +0200

    Solo solve 1500 rated problem

--- cf_1056c.cpp
+++ cf_1056c.cpp
@@ -14,16 +14,90 @@ int main() {
             cin >> a[i];
         }
 
-
-        // if I have a number a[i] at the end of the vector, then there must be at least a[i]-1 L values
-        // in the array and I have to have at least a[0]-1 Rs in the vector. Does that help me?
+        // a_{i+1} - a_{i} ==  1 => same and prev was L
+        // a_{i+1} - a_{i} == -1 => same and prev was R
         //
-        // if a[n-1] + a[0] - 2 > n, it means that the sum of L values and R values is not compatible with the array size, so 0.
-        // I will continue tomorrow
+        // |a_{i+1} - a_{i}| == 0 => different
 
-        // Submited not to lose my streak
+        int count = 0;
 
-        cout << "I don't know yet" << '\n';
+        string cand1;
+        string cand2;
+        cand1.resize(n);
+        cand2.resize(n);
+
+        for (int i = 0; i < n; ++i) cand1[i] = '?';
+        for (int i = 0; i < n; ++i) cand2[i] = '?';
+
+        cand1[0] = 'L';
+        cand2[0] = 'R';
+
+        for (int i = 0; i < n-1; ++i) {
+            if (a[i+1] - a[i] == 1) {
+                cand1[i+1] = 'L';
+                cand2[i+1] = 'L';
+            } else if (a[i+1] - a[i] == -1) {
+                cand1[i+1] = 'R';
+                cand2[i+1] = 'R';
+            } else if (a[i+1] - a[i] == 0) {
+                cand1[i+1] = cand1[i] == 'L' ? 'R' : 'L';
+                cand2[i+1] = cand2[i] == 'R' ? 'L' : 'R';
+            } else {
+                count = -1;
+                break;
+            }
+        }
+
+        if (count == -1) {
+            cout << 0 << '\n';
+            continue;
+        }
+
+        count = 2;
+        vector<int> prefixL(n);
+        vector<int> sufixR(n);
+
+        prefixL[0] = cand1[0] == 'L';
+        sufixR[n-1] = cand1[n-1] == 'R';
+
+        for (int i = 1; i < n; ++i) {
+            prefixL[i] = prefixL[i-1] + (cand1[i] == 'L');
+        }
+
+        for (int i = n-2; i >= 0; --i) {
+            sufixR[i] = sufixR[i+1] + (cand1[i] == 'R');
+        }
+
+        for (int i = 0; i < n; ++i) {
+            if (sufixR[i] + prefixL[i] != a[i]) {
+                count--;
+                break;
+            }
+        }
+
+        fill(prefixL.begin(), prefixL.end(), 0);
+        fill(sufixR.begin(), sufixR.end(), 0);
+
+        prefixL[0] = cand2[0] == 'L';
+        sufixR[n-1] = cand2[n-1] == 'R';
+
+        for (int i = 1; i < n; ++i) {
+            prefixL[i] = prefixL[i-1] + (cand2[i] == 'L');
+        }
+
+        for (int i = n-2; i >= 0; --i) {
+            sufixR[i] = sufixR[i+1] + (cand2[i] == 'R');
+        }
+
+        for (int i = 0; i < n; ++i) {
+            if (sufixR[i] + prefixL[i] != a[i]) {
+                count--;
+                break;
+            }
+        }
+
+        cout << count << '\n';
+        //cout << "I don't know yet" << '\n';
     }
     return 0;
 }
