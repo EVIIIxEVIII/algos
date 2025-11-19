68ca0f7cea93b6b50536dffe8458f6a4e78f3ab1
Kirill Katz <xnlyre@protonmail.com>
2025-11-19 21:53:49 +0200

    Solve another one

--- cf_1062d.cpp
+++ cf_1062d.cpp
@@ -29,11 +29,15 @@ int main() {
 
         ans = INT_MAX;
 
+        // 2 * 3 * 5 * 7 * 11 * 13 * 17 * 19 * 23 * 29 * 31 * 37 * 41 * 43 * 47 * 53 > 10^8
+        // so the greatest possible answer is 53
+
+        static long long primes[] = {2 , 3 , 5 , 7 , 11 , 13 , 17 , 19 , 23 , 29 , 31 , 37 , 41 , 43 , 47 , 53};
+
         for (int i = 0; i < n; ++i) {
-            for (long long j = 3; j <= a[i]; j+=2) {
-                if (a[i] % j != 0) {
-                    ans = min(j, ans);
-                    break;
+            for (int j = 0; j < 16; ++j) {
+                if (a[i] % primes[j] != 0) {
+                    ans = min(primes[j], ans);
                 }
             }
         }
