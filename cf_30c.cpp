de0bdaee6501c8fcf871e0f8a6a89f064d279071
Kirill Katz <xnlyre@protonmail.com>
2025-11-26 21:10:32 +0200

    Another one

--- cf_30c.cpp
+++ cf_30c.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <queue>
 using namespace std;
 
 int main() {
@@ -25,39 +26,68 @@ int main() {
             cin >> c[i];
         }
 
-        int maxSword = 0;
-        int maxPotentialSword = 0;
-
-        for (int i = 0; i < n; ++i) {
-            maxSword = max(maxSword, a[i]);
-            maxPotentialSword = max(maxPotentialSword, c[i]);
-        }
-
-        vector<int> idx(c.size());
+        vector<int> idx(b.size());
         iota(idx.begin(), idx.end(), 0);
         sort(idx.begin(), idx.end(), [&](int i, int j) {
-            return c[i] < c[j];
+            return b[i] < b[j];
         });
 
-        vector<int> c_sorted;
         vector<int> b_sorted;
+        vector<int> c_sorted;
 
         for (int i : idx) {
-            c_sorted.push_back(c[i]);
             b_sorted.push_back(b[i]);
+            c_sorted.push_back(c[i]);
         }
 
+        std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
+        for (int i = 0; i < n; ++i) {
+            pq.push(a[i]);
+        }
+
+        int ans = 0;
+        vector<int> stage2;
         for (int i = 0; i < m; ++i) {
-            cout << b_sorted[i] << ' ';
-        }
-        cout << '\n';
+            if (c_sorted[i] == 0) continue;
 
+            while (!pq.empty()) {
+                int sword = pq.top();
+                pq.pop();
+
+                if (sword >= b_sorted[i]) {
+                    int newSword = max(sword, c_sorted[i]);
+                    pq.push(newSword);
+                    b_sorted[i] = INT_MAX;
+                    ans++;
+                    break;
+                } else {
+                    stage2.push_back(sword);
+                }
+            }
+        }
+
+        while(!pq.empty()) {
+            stage2.push_back(pq.top());
+            pq.pop();
+        }
+
+        sort(stage2.begin(), stage2.end());
+        sort(b_sorted.begin(), b_sorted.end());
+
+        int j = 0;
         for (int i = 0; i < m; ++i) {
-            cout << c_sorted[i] << ' ';
+            while (j < n) {
+                int sword = stage2[j];
+                j++;
+
+                if (sword >= b_sorted[i]) {
+                    ans++;
+                    break;
+                }
+            }
         }
-        cout << '\n';
 
-
+        cout << ans << '\n';
     }
 
     return 0;
