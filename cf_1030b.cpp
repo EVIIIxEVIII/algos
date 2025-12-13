0a4859dd5856921f28567ba80fd95fb2e068abca
Kirill Katz <xnlyre@protonmail.com>
2025-12-13 11:54:20 +0200

    Another one

--- cf_1030b.cpp
+++ cf_1030b.cpp
@@ -5,20 +5,23 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    // key point: the next row should be a "shift" of the prior one
+
     int t; cin >> t;
     while (t--) {
         int n; cin >> n;
 
         cout << (n-2)*2 + 1 << '\n';
 
-        for (int i = 2; i < n; ++i) {
+        cout << 1 << ' ' << 1 << ' ' << n << '\n';
+        cout << 2 << ' ' << 2 << ' ' << n << '\n';
+
+        for (int i = 3; i < n; ++i) {
+            cout << i << ' ' << 1 << ' ' << i-1 << '\n';
             cout << i << ' ' << i << ' ' << n << '\n';
-            cout << i << ' ' << 1 << ' ' << n << '\n';
         }
 
-        cout << n << ' ' << 1 << ' ' << n << '\n';
-
-
+        cout << n << ' ' << 1 << ' ' << n-1 << '\n';
     }
     return 0;
 }
