e743574891d40f6ecddda1919362f4499b5ef010
Kirill Katz <xnlyre@protonmail.com>
2025-12-18 20:48:57 +0200

    Another one

--- cf_1055c.cpp
+++ cf_1055c.cpp
@@ -10,28 +10,49 @@ int main() {
         int n, q;
         cin >> n >> q;
 
-        vector<int> a(n);
-        for (int i = 0; i < n; ++i) cin >> a[i];
+        vector<int> a(n+1);
+        for (int i = 1; i <= n; ++i) {
+            cin >> a[i];
+        }
 
-        vector<int> l(q);
-        vector<int> r(q);
+        vector<int> l(q+1);
+        vector<int> r(q+1);
 
-        for (int i = 0; i < q; ++i) {
+        for (int i = 1; i <= q; ++i) {
             cin >> l[i];
             cin >> r[i];
         }
 
-        vector<int> prefix(n);
-        prefix[0] = a[0];
-
-        for (int i = 1; i < n; ++i) {
-            prefix[i] = a[i] + prefix[i-1];
+        vector<int> prefixOnes(n+1);
+        prefixOnes[1] = a[1];
+        for (int i = 2; i <= n; ++i) {
+            prefixOnes[i] = prefixOnes[i-1] + (a[i] == 1);
         }
 
-        for (int i = 0; i < q; ++i) {
-
+        vector<int> duplicatePrefix(n+1);
+        duplicatePrefix[1] = 0;
+        for (int i = 2; i <= n; ++i) {
+            duplicatePrefix[i] = duplicatePrefix[i-1];
+            if (a[i] == a[i-1]) {
+                duplicatePrefix[i]++;
+            }
         }
 
+        for (int i = 1; i <= q; ++i) {
+            if (
+                (prefixOnes[r[i]] - prefixOnes[l[i] - 1]) % 3 != 0 ||
+                ((r[i] - l[i] + 1) - (prefixOnes[r[i]] - prefixOnes[l[i] - 1])) % 3 != 0
+            ) {
+                cout << -1 << '\n';
+                continue;
+            }
+
+            if (duplicatePrefix[r[i]] - duplicatePrefix[l[i]] != 0) {
+                cout << (r[i] - l[i] + 1) / 3 << '\n';
+            } else {
+                cout << (r[i] - l[i] + 1) / 3 + 1 << '\n';
+            }
+        }
 
     }
     return 0;
