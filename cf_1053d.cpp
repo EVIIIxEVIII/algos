cd842e7475d209b7ef35cc4c21eabfa293d2af9b
Kirill Katz <xnlyre@protonmail.com>
2026-01-10 12:51:57 +0200

    Another one

--- cf_1053d.cpp
+++ cf_1053d.cpp
@@ -14,33 +14,6 @@ int main() {
             cin >> a[i];
         }
 
-        // ok, so the new idea goes something like this:
-        // we basically have a pyramid, but turned down
-        // on the first row we can place the value in any cell, but we must put them in the
-        // corners, because this is the only row which supports values (1, n) (n, 1)
-        // and every other row suppors n - 2 values in the center
-        // but you can place a value for some column at a row lower
-        //
-        // so the "rigid" configuration is when we have exactly 2 on each row besides the last
-        // one where we have (n % 2 == 0) black cells
-        //
-        // ok, what's next?
-        //
-        // so if we have exactly 2 on each row, then we have the rigid case
-        // buf if we have an additional one, we get some room for change
-        // let's say we have the 5 by 5 case with 1 additional cell on the first row,
-        // this means that we can move that cell 3 times
-        // so if there is a free cell I can move it n - i*2 number of times
-        // or more generall n - i*2 choose k (probably) where k is the number of these "free" cells we have on row i
-        // but we might have a rigid configuration at the top of the pyramid, so we have to take into account that
-        // so something like n - i*2 choose k - rigid cells, where k = 0
-        //
-        // but we know that we must have 2 cells on each row, besides the last one where it depends on the parity,
-        // so if we have k cells on row i, then it means we have k less rows on the next few rows
-        //
-        // I don't know how to count these. I completely understand the problem.
-        // We have a pyramid that is turned upside down and on each column we can choose any cell
-        // the problem is the fixed number of rows, which dictates where what column we must select the cell in.
 
         long long sum = 0;
         for (int i = 0; i < n; ++i) {
---
+++ cf_2034c.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+
+        // main idea: sometimes it is useful to flip the question, because that makes
+        // solving the problem much easier.
+
+        vector<vector<bool>> undef(n+2, vector<bool>(m+2, false));
+        vector<vector<vector<pair<int, int>>>> graph(n+2, vector<vector<pair<int, int>>>(m+2));
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                char dir; cin >> dir;
+
+                int ni = i, nj = j;
+
+                if (dir == 'U') {
+                    ni--;
+                } else if (dir == 'D') {
+                    ni++;
+                } else if (dir == 'R') {
+                    nj++;
+                } else if (dir == 'L') {
+                    nj--;
+                } else if (dir == '?') {
+                    undef[i][j] = true;
+                }
+
+                graph[ni][nj].push_back({i, j});
+            }
+        }
+
+        long long ans = 0;
+        vector<vector<bool>> reachable(n+2, vector<bool>(m+2));
+        auto dfs = [&](auto&& self, int i, int j) -> void {
+            if (reachable[i][j]) return;
+            reachable[i][j] = true;
+
+            for (auto& [ni, nj] : graph[i][j]) {
+                self(self, ni, nj);
+            }
+        };
+
+        for (int j = 1; j <= m; ++j) {
+            dfs(dfs, 0, j);
+        }
+
+        for (int j = 1; j <= m; ++j) {
+            dfs(dfs, n+1, j);
+        }
+
+        for (int i = 1; i <= n; ++i) {
+            dfs(dfs, i, 0);
+        }
+
+        for (int i = 1; i <= n; ++i) {
+            dfs(dfs, i, m+1);
+        }
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (undef[i][j]) {
+                    if (
+                        !reachable[i+1][j] ||
+                        !reachable[i-1][j] ||
+                        !reachable[i][j+1] ||
+                        !reachable[i][j-1]
+                    ) {
+                        ans++;
+                    }
+                } else if (!reachable[i][j]) {
+                    ans++;
+                }
+            }
+        }
+
+        cout << ans << '\n';
+    }
+    return 0;
+}
