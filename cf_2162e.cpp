4199209a6f61fbde14374cc21bb49dd227d3f23f
Kirill Katz <xnlyre@protonmail.com>
2026-01-25 19:58:59 +0200

    Fix

--- cf_2162e.cpp
+++ cf_2162e.cpp
@@ -11,16 +11,11 @@ int main() {
         cin >> n >> k;
 
         vector<int> a(n);
+        vector<int> cnt(n+1, 0);
 
         for (int i = 0; i < n; ++i) {
             cin >> a[i];
-        }
-        bool equal = true;
-        for (int i = 0; i < n-1; ++i) {
-            if (a[i] != a[i+1]) {
-                equal = false;
-                break;
-            }
+            cnt[a[i]]++;
         }
 
         // idea: it is not possible to decrease the existent number of palindromic subarrays, but it
@@ -42,33 +37,39 @@ int main() {
         // we would be good to go, but the first two elements should be chosen such that el1 != a[n] && el != a[n-1] && el2 !=
         // a[n] && el2 != a[n-1]
 
-        list<int> free_list;
+
+        int x = -1;
         for (int i = 1; i <= n; ++i) {
-            free_list.push_back(i);
-        }
-
-        int not_equal = a[n-1];
-        int not_equal_prev= a[n-2];
-        int count = 0;
-
-        for (auto it = free_list.begin(); it != free_list.end() && count < 2;) {
-            if (*it != not_equal && *it != not_equal_prev) {
-                cout << *it << ' ';
-                it = free_list.erase(it);
-                count++;
-            } else {
-                ++it;
+            if (cnt[i] == 0) {
+                x = i;
+                break;
             }
         }
 
-        if (k == n && equal) {
-            free_list.push_back(a[0]); // if everything is equal we wouldn't be able to have
-            // all elements such that a[n-i-1] != ans[i]; so we add one more a[0] at the end
-        }
+        if (x == -1) {
+            for (int i = 0; i < k; ++i) {
+                cout << a[i % 3] << ' ';
+            }
+        } else {
+            int y = -1;
+            int z = a[n-1];
 
-        for (int i = 2; i < k; ++i) {
-            cout << free_list.front() << ' ';
-            free_list.pop_front();
+            for (int i = 1; i <= n;  ++i) {
+                if (i != x && i != z) {
+                    y = i;
+                    break;
+                }
+            }
+
+            for (int i = 0; i < k; ++i) {
+                if (i % 3 == 0) {
+                    cout << x << ' ';
+                } else if (i % 3 == 1) {
+                    cout << y << ' ';
+                } else {
+                    cout << z << ' ';
+                }
+            }
         }
 
         cout << '\n';
