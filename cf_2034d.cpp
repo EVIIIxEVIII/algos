c52a02ba106e46bab5220cfba3f3d393e1cdcec5
Kirill Katz <xnlyre@protonmail.com>
2026-02-17 18:06:16 +0200

    another one

--- cf_2034d.cpp
+++ cf_2034d.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <queue>
 using namespace std;
 
 int main() {
@@ -12,53 +13,91 @@ int main() {
         // then we would know that the first a positions should be 0, the next b should be 1 and the last c should be 2
         // idea: notice that 1s can be swapped with both 0s and 2s, so we should start with them and then we could use 1s
         // to swap 0s with 2s in 2 moves.
+        //
+        // idea: we need only one 1 and use it as a bridge to sort 0s and 2s.
+        // first we can take a 2 in the 0 segment put them in the 2 segment
+        //
+        // idea: we only need to fix the 0s segment after than it is 1 move
+        // per exchange
+        //
+        // idea:
+        //
+        // 0 2 1 0 -> 0 1 2 0
 
         int n;
         cin >> n;
+
         vector<int> a(n);
         int z = 0, o = 0, t = 0;
 
+        vector<pair<int, int>> ans;
+
         for (int i = 0; i < n; ++i) {
             cin >> a[i];
+
             if (a[i] == 0) z++;
             if (a[i] == 1) o++;
             if (a[i] == 2) t++;
         }
 
-        vector<pair<int, int>> ans;
-        set<int> free_zeros;
+        set<int> zeros;
+        for (int i = z; i < n; ++i) {
+            if (a[i] == 0) {
+                zeros.insert(i);
+            }
+        }
 
         for (int i = 0; i < z; ++i) {
-            if (a[i] != 1 && a[i] != 0) {
-                free_zeros.insert(i);
+            if (a[i] == 1) {
+                int last = *prev(zeros.end());
+                ans.push_back({ i+1, last + 1 });
+                a[last] = 1;
+                a[i] = 0;
+                zeros.erase(last);
             }
         }
 
-        for (int i = 0; i < n; ++i) {
-            if (a[i] != 1 || i < z) continue;
-
-            if (!free_zeros.empty()) {
-                ans.push_back({ *free_zeros.begin() + 1, i + 1 });
-                free_zeros.erase(*free_zeros.begin());
-            } else break;
-        }
-
-        set<int> ones_pos;
-        for (int i = 0, swapped = 0; i < n; ++i) {
-            if (a[i] != 0) continue;
-
-            ans.push_back({ ++swapped, i+1 });
-
-            if (i >= o) {
-                ones_pos.insert(i);
+        set<int> ones;
+        for (int i = z; i < n; ++i) {
+            if (a[i] == 1) {
+                ones.insert(i);
             }
         }
 
-        for (int i = z; i < o; ++i) {
-            if (a[i] != 2) continue;
+        for (int i = 0; i < z; ++i) {
+            if (a[i] == 2) {
+                int last_one = *prev(ones.end());
+                ans.push_back({ i + 1, last_one + 1 });
+                ones.erase(last_one);
 
-            ans.push_back({ *ones_pos.begin()+1, i+1 });
-            ones_pos.erase(*ones_pos.begin());
+                int last_zero = *prev(zeros.end());
+                ans.push_back({ i + 1, last_zero + 1 });
+                zeros.erase(last_zero);
+
+                a[last_one] = 2;
+                a[last_zero] = 1;
+                a[i] = 0;
+
+                ones.insert(last_zero);
+            }
+        }
+
+        vector<int> ones_;
+        vector<int> twos_;
+        for (int i = z; i < z+o; ++i) {
+            if (a[i] == 2) {
+                twos_.push_back(i);
+            }
+        }
+
+        for (int i = z+o; i < n; ++i) {
+            if (a[i] == 1) {
+                ones_.push_back(i);
+            }
+        }
+
+        for (size_t i = 0; i < ones_.size(); ++i) {
+            ans.push_back({ ones_[i] + 1, twos_[i] + 1 });
         }
 
         cout << ans.size() << '\n';
@@ -66,5 +105,6 @@ int main() {
             cout << ans[i].first << ' ' << ans[i].second << '\n';
         }
     }
+
     return 0;
 }
