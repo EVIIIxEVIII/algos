3adf7e62f1a69db1c175c1680613c9be5c460c9c
Kirill Katz <xnlyre@protonmail.com>
2026-03-08 21:44:59 +0200

    Try

---
+++ cf_2121e.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve();
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+void solve() {
+    long long l, r;
+    cin >> l >> r;
+
+    int num = l;
+    int n = 0;
+    int exp = 1;
+
+    while (num > 0) {
+        num /= 10;
+        exp *= 10;
+        n++;
+    }
+    exp/=10;
+
+    long long ans = 0;
+    for (int i = 0; i < n; ++i) {
+        int dl = (l / exp) % 10;
+        int dr = (r / exp) % 10;
+
+        if (dl == dr) {
+            ans += 2;
+        } else if (abs(dl - dr) == 1 || (max(dl, dr) == 9 && min(dl, dr) == 0)) {
+            ans++;
+        } else break;
+
+        exp /= 10;
+    }
+
+    cout << ans << '\n';
+}
