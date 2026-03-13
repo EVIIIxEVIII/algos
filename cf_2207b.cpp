54724adfa772bf925f2c713046f39ce164b1e10a
Kirill Katz <xnlyre@protonmail.com>
2026-03-13 23:01:58 +0200

    what a garbage problem

--- cf_2207b.cpp
+++ cf_2207b.cpp
@@ -17,55 +17,37 @@ void solve() {
     int n, m, l;
     cin >> n >> m >> l;
 
-    vector<int> a(n+2);
-    a[0] = 0;
-    for (int i = 1; i <= n; ++i) {
+    vector<int> a(n+1);
+    for (int i = 0; i < n; ++i) {
         cin >> a[i];
     }
-    a[n+1] = l;
+    a[n] = l;
 
-    // probably a b-search problem, but how do I do the check function?
-    // probably not even a b-search problem because the check requires you to know
-    // the best strategy already.
+    vector<int> dis(n+1);
+    dis[0] = a[0];
 
-    // at each step you can either divide between all the cells or add the distance to a single cell
+    for (int i = 1; i <= n; ++i) {
+        dis[i] = a[i] - a[i-1];
+    }
+
+    // maybe only the last m a[i] matter? because the last segment should be split into 2 danger levels
+    // the one before that should be split into 3 danger levels and the one before into 4 danger levels.
+    // This is because one of the values will be destoroyed anyways after each a[i], so it would make sense to
+    // split the danger levels in this way.
 
     long long ans = 0;
-    vector<int> d(m);
-    for (int i = 1; i <= n+1; ++i) {
-        int max_val_before = 0;
 
-        int per = (a[i] - a[i-1]) / m;
-        int rem = (a[i] - a[i-1]) % m;
 
-        for (int j = 0; j < m; ++j) {
-            max_val_before = max(d[j], max_val_before);
-            d[j] += per;
-        }
+    for (int i = n; i >= max(n - m + 2, 0); --i) {
+        int div = n - i + 1;
 
-        for (int j = 0; j < m; ++j) {
-            if (d[j] < max_val_before + per && rem > 0) {
-                d[j] += 1;
-                rem--;
-            }
-        }
-
-        for (int j = 0; j < rem; ++j) {
-            d[j]++;
-        }
-
-        int max_j = 0;
-        int max_val_after = 0;
-        for (int j = 0; j < m; ++j) {
-            if (max_val_after < d[j]) {
-                max_val_after = d[j];
-                max_j = j;
-            }
-        }
-
-        d[max_j] = 0;
-        ans = max(max_val_before * 1LL + a[i], ans);
+        ans += dis[i] / div;
     }
+
+    for (int i = 0; i <= n; ++i) {
+        cout << dis[i] << ' ';
+    }
+    cout << '\n';
 
     cout << ans << '\n';
 }
