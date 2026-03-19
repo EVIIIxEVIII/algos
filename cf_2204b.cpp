0f0942591bde9dd1eee6ae62ba6cf337a3eb82e7
Kirill Katz <xnlyre@protonmail.com>
2026-03-19 14:34:59 +0200

    Another onoe

--- cf_2204b.cpp
+++ cf_2204b.cpp
@@ -14,5 +14,39 @@ int main() {
 }
 
 void solve() {
+    int n; cin >> n;
 
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    vector<int> cnt(n+1);
+    int ans = 0;
+
+    int current = 0;
+    for (int i = 0; i < n; ++i) {
+        cnt[a[i]]++;
+        current = max(current, a[i]);
+    }
+
+    for (int i = n - 1; i >= 0;) {
+        if (current == 0) break;
+
+        if (cnt[current]) {
+            while (i >= 0 && cnt[current] > 0) {
+                if (a[i] == current) {
+                    cnt[current]--;
+                    ans++;
+                } else {
+                    cnt[a[i]]--;
+                }
+                i--;
+            }
+        } else {
+            current--;
+        }
+    }
+
+    cout << ans << '\n';
 }
