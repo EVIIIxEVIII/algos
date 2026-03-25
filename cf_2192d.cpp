ae699bf4f005e91cd75f565dba5ebbaa6c496720
Kirill Katz <xnlyre@protonmail.com>
2026-03-25 15:46:32 +0200

    Another one

--- cf_2192d.cpp
+++ cf_2192d.cpp
@@ -1,95 +1,107 @@
-#include <algorithm>
 #include <bits/stdc++.h>
 using namespace std;
+
+void solve();
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int t; cin >> t;
-    while (t--) {
-        // idea: precompute the sum of a_u * d(r, u) for each node, then for each subtree with root r we need to find
-        // the node that is not a part of that subtree but which is the most distant from the root.
+    while (t--) solve();
 
-        // idea: do a dfs and each node will return a tuple of 3 elements: [sum, weighted_sum, max_depth]
-        // where sum is the sum of a[i] is all of its children and weighted_sum is the total weighted sum including
-        // the depth of each child.
+    return 0;
+}
 
-        int n;
-        cin >> n;
+void solve() {
+    int n;
+    cin >> n;
 
-        vector<long long> a(n+1);
-        for (int i = 1; i <= n; ++i) {
-            cin >> a[i];
+    vector<long long> a(n+1);
+    for (int i = 1; i <= n; ++i) {
+        cin >> a[i];
+    }
+
+    vector<vector<int>> tree(n+1);
+    for (int i = 0; i < n - 1; ++i) {
+        int u, v;
+        cin >> u >> v;
+        tree[u].push_back(v);
+        tree[v].push_back(u);
+    }
+    tree[1].push_back(1);
+
+    // it probably makes sense to do the operation on the first split with respect to r, where we
+    // take one subtree from the split and add it to another subtree.
+
+    vector<long long> depth(n+1);
+    vector<long long> score(n+1);
+    vector<long long> sum(n+1);
+    vector<long long> ans(n+1);
+
+    auto dfs = [&](auto&& self, int v, int p) -> void {
+        if (tree[v].size() == 1 && v != 1) {
+            ans[v] = 0LL;
+            depth[v] = 1LL;
+            sum[v] = a[v];
+            score[v] = 0LL;
+            return;
         }
 
-        vector<vector<int>> tree(n+1);
-        for (int i = 0; i < n - 1; ++i) {
-            int u, v;
-            cin >> u >> v;
-            tree[u].push_back(v);
-            tree[v].push_back(u);
+        long long max_d = 0;
+        long long max_d_p = 0;
+
+        long long total_score = 0;
+        long long total_sum = 0;
+
+        for (int u : tree[v]) {
+            if (u == p) continue;
+
+            self(self, u, v);
+            total_score += score[u];
+            total_sum += sum[u];
+
+            if (depth[u] > max_d) {
+                max_d_p = max_d;
+                max_d = depth[u];
+            } else if (depth[u] > max_d_p) {
+                max_d_p = depth[u];
+            }
         }
 
-        struct Res {
-            long long sum;
-            long long weighted_sum;
-            long long max_depth;
-        };
+        total_score += total_sum;
 
-        vector<long long> sum(n + 1);
-        vector<long long> w_sum(n + 1);
-        vector<long long> depth(n + 1);
-        vector<long long> best_child_gain(n + 1);
-
-        vector<long long> ans_vec(n+1);
-        auto dfs = [&](auto&& self, int u, int p) -> Res {
-            Res res = { a[u], 0, 0 };
-            long long max_depth = 0;
-            long long max_depth_prev = 0;
-            int max_depth_child = 0;
-
-            best_child_gain[u] = 0;
-            for (auto& v : tree[u]) {
-                if (v == p) continue;
-
-                auto [ s, ws, d ] = self(self, v, u);
-                best_child_gain[u] = max(best_child_gain[u], best_child_gain[v]);
-
-                if (d + 1 > max_depth) {
-                    max_depth_prev = max_depth;
-                    max_depth = d + 1;
-                    max_depth_child = v;
+        if (tree[v].size() == 2) {
+            int only_child = tree[v][0] != p ? tree[v][0] : tree[v][1];
+            ans[v] = ans[only_child] + sum[only_child];
+        } else {
+            long long answer = 0;
+            for (int u : tree[v]) {
+                if (u == p) continue;
+                long long d = max_d;
+                if (depth[u] == max_d) {
+                    d = max_d_p;
                 }
 
-                res.sum += s;
-                res.weighted_sum += ws + s;
-                res.max_depth = max(res.max_depth, d + 1);
+                answer = max({
+                    answer,
+                    total_score + sum[u]*d,
+                    total_score - score[u] + ans[u]
+                });
             }
 
-            long long gain = 0;
-            for (auto& v : tree[u]) {
-                if (v == p) continue;
-                long long D = (v == max_depth_child ? max_depth_prev : max_depth);
-                gain = max(gain, D * sum[v]);
-                best_child_gain[u] = max(best_child_gain[u], gain);
-            }
-
-            ans_vec[u] = res.weighted_sum + best_child_gain[u];
-
-            sum[u] = res.sum;
-            w_sum[u] = res.weighted_sum;
-            depth[u] = res.max_depth;
-
-            return res;
-        };
-
-        dfs(dfs, 1, 1);
-
-        for (int i = 1; i <= n; ++i) {
-            cout << ans_vec[i] << ' ';
+            ans[v] = answer;
         }
-        cout << '\n';
+
+        depth[v] = max_d + 1LL;
+        score[v] = total_score;
+        sum[v] = total_sum + a[v];
+    };
+
+    dfs(dfs, 1, 1);
+    for (int i = 1; i <= n; ++i) {
+        cout << ans[i] << ' ';
     }
-    return 0;
+
+    cout << '\n';
 }
