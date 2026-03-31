e0faafb90f59d6231566e41449882330825c50f2
Kirill Katz <xnlyre@protonmail.com>
2026-03-31 22:13:35 +0300

    change

--- cf_2211c1.cpp
+++ cf_2211c1.cpp
@@ -27,70 +27,34 @@ void solve() {
         cin >> b[i];
     }
 
-    deque<int> win_a;
-    deque<int> win_b;
-
-    set<int> win_a_els;
-    set<int> win_b_els;
-
-    for (int i = 0; i < k; ++i) {
-        win_a.push_back(a[i]);
-        win_a_els.insert(a[i]);
-
-        win_b.push_back(b[i]);
-        if (b[i] != -1) {
-            auto it = win_b_els.insert(b[i]);
-            if (!it.second) {
-                cout << "NO" << '\n';
-                return;
-            }
-
-            if (!win_a_els.contains(b[i])) {
-                cout << "NO" << '\n';
-                return;
-            }
+    for (int i = 0; i < n - k; ++i) {
+        if (b[i] != -1 && b[i] != a[i]) {
+            cout << "NO" << '\n';
+            return;
         }
     }
 
-    int r = k;
-    while (r < n) {
-        int rem_a = win_a.front();
-        win_a.pop_front();
-
-        int rem_b = win_b.front();
-        win_b.pop_front();
-
-        win_a_els.erase(rem_a);
-        if (rem_b != -1) win_b_els.erase(rem_b);
-
-        if (win_b_els.contains(rem_a)) {
+    for (int i = k; i < n; ++i) {
+        if (b[i] != -1 && b[i] != a[i]) {
             cout << "NO" << '\n';
             return;
         }
+    }
 
-        if (win_a_els.contains(rem_b)) {
-            cout << "NO" << '\n';
-            return;
-        }
+    set<int> set_a;
+    set<int> set_b;
 
-        win_a.push_back(a[r]);
-        win_b.push_back(b[r]);
+    for (int i = n - k; i < k; ++i) {
+        set_a.insert(a[i]);
+    }
 
-        win_a_els.insert(a[r]);
-        if (b[r] != -1) {
-            auto it = win_b_els.insert(b[r]);
-            if (!it.second) {
-                cout << "NO" <<'\n';
-                return;
-            }
-
-            if (!win_a_els.contains(b[r])) {
+    for (int i = n - k; i < k; ++i) {
+        if (b[i] != -1) {
+            if (!set_a.contains(b[i]) || !set_b.insert(b[i]).second) {
                 cout << "NO" << '\n';
                 return;
             }
         }
-
-        r++;
     }
 
     cout << "YES" << '\n';
