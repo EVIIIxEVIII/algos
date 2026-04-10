5ab3e8138da55a3806653dcc61155f3e28249b71
Kirill Katz <xnlyre@protonmail.com>
2026-04-10 14:04:56 +0300

    solve

--- cf_2059b.cpp
+++ cf_2059b.cpp
@@ -14,5 +14,29 @@ int main() {
 }
 
 void solve() {
+    int n, k; cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
 
+    if (k == n) {
+        for (int i = 1; i < n; i+=2) {
+            if (a[i] != i/2+1) {
+                cout << i/2+1 << '\n';
+                return;
+            }
+        }
+        cout << n/2 + 1 << '\n';
+        return;
+    }
+
+    int free_moves = n-k;
+    for (int i = 1; i <= free_moves+1; ++i) {
+        if (a[i] != 1) {
+            cout << 1 << '\n';
+            return;
+        }
+    }
+    cout << 2 << '\n';
 }
