60113a363894fac47af290681f0aeaf7a9bf2a5f
Kirill Katz <xnlyre@protonmail.com>
2026-04-15 22:49:28 +0300

    Solve

--- cf_2220b.cpp
+++ cf_2220b.cpp
@@ -14,5 +14,32 @@ int main() {
 }
 
 void solve() {
+    int n, m;
+    cin >> n >> m;
 
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+
+    int max_in_row = 1;
+    int in_row = 1;
+
+    for (int i = 1; i < n; ++i) {
+        if (a[i] == a[i-1]) {
+            in_row++;
+        } else {
+            max_in_row = max(max_in_row, in_row);
+            in_row = 1;
+        }
+    }
+
+    max_in_row = max(in_row, max_in_row);
+
+    if (max_in_row < m) {
+        cout << "YES" << '\n';
+    } else {
+        cout << "NO" << '\n';
+    }
 }
--- cf_2220c.cpp
+++ cf_2220c.cpp
@@ -14,5 +14,27 @@ int main() {
 }
 
 void solve() {
+    long long p, q;
+    cin >> p >> q;
 
+    long long x = p + 2*q;
+    long long target = 2*x + 1;
+
+    long long div = 2;
+
+    for (long long d = 1; d * d <= target; d++) {
+        if (target % d != 0) continue;
+
+        long long e = target / d;
+
+        long long m = (d - 1) / 2;
+        long long n = (e - 1) / 2;
+
+        if (m > 0 && n > 0 && p >= abs(m - n)) {
+            cout << m << ' ' << n << '\n';
+            return;
+        }
+    }
+
+    cout << -1 << '\n';
 }
