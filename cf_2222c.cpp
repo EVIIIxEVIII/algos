ee877240fcbfee116ce62ceb12c7186abd0a01f1
Kirill Katz <xnlyre@protonmail.com>
2026-04-26 10:46:34 +0300

    Resolve

--- cf_2222c.cpp
+++ cf_2222c.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#include <climits>
 using namespace std;
 
 void solve();
@@ -15,39 +14,44 @@ int main() {
 }
 
 void solve() {
-    int n; cin >> n;
+    int n;
+    cin >> n;
 
     vector<int> a(n);
     for (int i = 0; i < n; ++i) {
         cin >> a[i];
     }
 
-    vector<int> c = a;
-    sort(c.begin(), c.end());
-    int target = c[n/2];
+    vector<int> s = a;
+    sort(s.begin(), s.end());
 
-    vector<long long> pref_ge(n + 1, 0);
-    vector<long long> pref_le(n + 1, 0);
-    for (int i = 0; i < n; ++i) {
-        pref_ge[i+1] = pref_ge[i] + (a[i] >= target ? 1 : -1);
-        pref_le[i+1] = pref_le[i] + (a[i] <= target ? 1 : -1);
+    int T = s[n/2];
+
+    vector<long long> pref_ge(n+1);
+    vector<long long> pref_le(n+1);
+
+    pref_ge[0] = 0;
+    pref_le[0] = 0;
+
+    for (int i = 1; i <= n; ++i) {
+        pref_ge[i] = pref_ge[i-1] + (a[i - 1] >= T ? 1 : -1);
+        pref_le[i] = pref_le[i-1] + (a[i - 1] <= T ? 1 : -1);
     }
 
-    constexpr long long NEG_INF = -1e18;
-    vector<long long> dp(n + 1, NEG_INF);
-
+    constexpr long long NEG_INF = - 1e18;
+    vector<long long> dp(n+1, NEG_INF);
     dp[0] = 0;
 
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; ++i) {
         for (int j = 0; j < i; ++j) {
             if ((i - j) % 2 == 0) {
                 continue;
             }
 
-            long long sum_ge = pref_ge[i] - pref_ge[j];
-            long long sum_le = pref_le[i] - pref_le[j];
+            long long ge_cnt = pref_ge[i] - pref_ge[j]; // in region [j, i-1]
+            long long le_cnt = pref_le[i] - pref_le[j];
 
-            if (sum_ge > 0 && sum_le > 0 && dp[j] != NEG_INF) {
+            if (dp[j] != NEG_INF && ge_cnt > 0 && le_cnt > 0) {
                 dp[i] = max(dp[i], dp[j] + 1);
             }
         }
--- cf_2222d.cpp
+++ cf_2222d.cpp
@@ -14,5 +14,13 @@ int main() {
 }
 
 void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
 
 }
