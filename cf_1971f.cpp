99afe291bf859555627c7d4facfdca40a68ab6c6
Kirill Katz <xnlyre@protonmail.com>
2026-05-04 19:34:36 +0300

    Solve

--- cf_1971f.cpp
+++ cf_1971f.cpp
@@ -20,5 +20,41 @@ int main() {
 }
 
 void solve() {
+    long long r;
+    cin >> r;
 
+    // the number of latice points stricly inside the circle with radius r + 1?
+    // x^2 + y^2 < (r+1)^2 => x^2 < (r+1)^2 - y^2 => x < sqrt((r+1)^2 - y^2)
+    // then go through 1 <= y < r and check for x? but multiply by 2 for the symmetric thing on the other side?
+
+    long long total = 0;
+    for (long long y = 0; y < (r+1); ++y) {
+        long long val = (r+1)*(r+1) - y*y;
+        long long xes = sqrt(val - 1);
+
+        long long row = xes * 2 + 1;
+        if (y == 0) {
+            total += row;
+        } else {
+            total += row * 2;
+        }
+    }
+    total--;
+
+    long long exclude = 0;
+    for (long long y = 0; y < r; ++y) {
+        long long val = r * r - y * y;
+        long long xes = sqrt(val - 1);
+
+        long long row = xes * 2 + 1;
+
+        if (y == 0) {
+            exclude += row;
+        } else {
+            exclude += row * 2;
+        }
+    }
+    exclude--;
+
+    cout << total - exclude << '\n';
 }
