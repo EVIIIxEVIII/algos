6f1c03b65efe4ff1e42c70474e4237b93d237b1f
Kirill Katz <xnlyre@protonmail.com>
2026-05-05 13:37:55 +0300

    Solve

--- cf_1946c.cpp
+++ cf_1946c.cpp
@@ -20,5 +20,51 @@ int main() {
 }
 
 void solve() {
+    int n, k;
+    cin >> n >> k;
 
+    vector<vector<int>> g(n+1);
+
+    for (int i = 0; i < n - 1; ++i) {
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    auto check = [&](int x) -> bool {
+        int times = 0;
+        auto dfs = [&](auto&& self, int v, int p) -> long long {
+            long long subtree_sz = 1;
+            for (int u : g[v]) {
+                if (u == p) continue;
+                subtree_sz += self(self, u, v);
+            }
+
+            if (subtree_sz >= x) {
+                times++;
+                return 0;
+            }
+
+            return subtree_sz;
+        };
+
+        dfs(dfs, 1, 0);
+        return times >= k + 1;
+    };
+
+    //TTFF
+    int l = 1;
+    int r = n;
+
+    while (l < r) {
+        int m = l + (r - l + 1) / 2;
+        if (check(m)) {
+            l = m;
+        } else {
+            r = m - 1;
+        }
+    }
+
+    cout << l << '\n';
 }
