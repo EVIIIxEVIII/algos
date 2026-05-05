fce55d46e8838c5347ad558ee1900cf1795a3977
Kirill Katz <xnlyre@protonmail.com>
2026-05-05 11:41:30 +0300

    Solve

--- cf_1968e.cpp
+++ cf_1968e.cpp
@@ -20,5 +20,24 @@ int main() {
 }
 
 void solve() {
+    int n;
+    cin >> n;
 
+    if (n == 2) {
+        cout << 1 << ' ' << 1 << '\n';
+        cout << 1 << ' ' << 2 << '\n';
+    } else if (n == 3) {
+        cout << 2 << ' ' << 1 << '\n';
+        cout << 2 << ' ' << 3 << '\n';
+        cout << 3 << ' ' << 1 << '\n';
+    } else {
+        for (int i = 1; i <= n - 2; ++i) {
+            cout << i << ' ' << i << '\n';
+        }
+
+        cout << n << ' ' << n - 1 << '\n';
+        cout << n << ' ' << n << '\n';
+    }
+
+    cout << '\n';
 }
