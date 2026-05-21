44a1ea2b467b95a34ceeb550b142519102ccea2e
Kirill Katz <xnlyre@protonmail.com>
2026-05-21 11:40:54 +0300

    Solve

--- cf_2096c.cpp
+++ cf_2096c.cpp
@@ -72,139 +72,85 @@ void solve() {
     // no matter how we try to fix that [2, 2] second row, we just aren't able to do so, because of the inverse [2, 1] and [1, 2]
     //
     // Thus column operations and row operations are basically independent of each other and can be checked separately.
+    //
+    // dp[0][i] stores the best cost up to column i if we don't fix column i
+    // dp[1][i] stores the best cost up to column i if we did fix it
 
-    vector<pair<int,int>> seg_col;
-    for (int i = 0; i < n; ++i) {
-        for (int l = 0; l < n;) {
-            int r = l;
-            while (r < n && h[i][l] == h[i][r]) {
-                r++;
+    const long long INF = LLONG_MAX / 4;
+    array<vector<long long>, 2> dp_col{ vector<long long>(n, INF), vector<long long>(n, INF) };
+
+    dp_col[0][0] = 0LL;
+    dp_col[1][0] = b[0];
+
+    auto check_col = [&](int j, int cur, int prev) {
+        for (int i = 0; i < n; ++i) {
+            if (h[i][j] + cur == h[i][j-1] + prev) {
+                return false;
             }
-
-            if (r - l >= 2) {
-                seg_col.push_back({l, r});
-            }
-
-            l = r;
-        }
-    }
-
-    sort(seg_col.begin(), seg_col.end());
-
-    vector<pair<int,int>> mer_seg_col;
-    for (int i = 0; i < (int)seg_col.size();) {
-        int l = seg_col[i].first;
-        int r = seg_col[i].second;
-        int j = i + 1;
-
-        while (j < (int)seg_col.size() && seg_col[j].first < r) {
-            r = max(r, seg_col[j].second);
-            j++;
         }
 
-        mer_seg_col.push_back({ l, r });
-        i = j;
-    }
-
-    auto check_col_seg = [&](bool even, int l, int r) -> long long {
-        if (even) l++;
-        long long cost = 0;
-
-        for (int j = l; j < r; j += 2) {
-            for (int i = 0; i < n; ++i) {
-                if (j - 1 >= 0 && h[i][j] + 1 == h[i][j-1]) {
-                    return LLONG_MAX;
-                }
-
-                if (j + 1 < n && h[i][j] + 1 == h[i][j + 1]) {
-                    return LLONG_MAX;
-                }
-            }
-
-            cost += b[j];
-        }
-
-        return cost;
+        return true;
     };
 
-    long long ans = 0;
-    for (auto& [l, r] : mer_seg_col) {
-        long long even = check_col_seg(true, l, r);
-        long long odd = check_col_seg(false, l, r);
-
-        if (even == LLONG_MAX && odd == LLONG_MAX) {
-            cout << -1 << '\n';
-            return;
+    for (int j = 1; j < n; ++j) {
+        if (check_col(j, 0, 0)) {
+            dp_col[0][j] = min(dp_col[0][j-1], dp_col[0][j]);
         }
 
-        ans += min(even, odd);
-    }
+        if (check_col(j, 1, 0)) {
+            dp_col[1][j] = min(dp_col[0][j-1] + b[j], dp_col[1][j]);
+        }
 
-    vector<pair<int,int>> seg_row;
-    for (int j = 0; j < n; ++j) {
-        for (int l = 0; l < n;) {
-            int r = l;
-            while (r < n && h[l][j] == h[r][j]) {
-                r++;
-            }
+        if (check_col(j, 0, 1)) {
+            dp_col[0][j] = min(dp_col[1][j-1], dp_col[0][j]);
+        }
 
-            if (r - l >= 2) {
-                seg_row.push_back({l, r});
-            }
-
-            l = r;
+        if (check_col(j, 1, 1)) {
+            dp_col[1][j] = min(dp_col[1][j-1] + b[j], dp_col[1][j]);
         }
     }
 
-    sort(seg_row.begin(), seg_row.end());
-
-    vector<pair<int,int>> mer_seg_row;
-    for (int i = 0; i < (int)seg_row.size();) {
-        int l = seg_row[i].first;
-        int r = seg_row[i].second;
-        int j = i + 1;
-
-        while (j < (int)seg_row.size() && seg_row[j].first < r) {
-            r = max(r, seg_row[j].second);
-            j++;
-        }
-
-        mer_seg_row.push_back({ l, r });
-        i = j;
+    if (dp_col[0][n-1] == INF && dp_col[1][n-1] == INF) {
+        cout << -1 << '\n';
+        return;
     }
 
-    auto check_row_seg = [&](bool even, int l, int r) -> long long {
-        if (even) l++;
-        long long cost = 0;
-
-        for (int i = l; i < r; i += 2) {
-            for (int j = 0; j < n; ++j) {
-                if (i - 1 >= 0 && h[i][j] + 1 == h[i - 1][j]) {
-                    return LLONG_MAX;
-                }
-
-                if (i + 1 < n && h[i][j] + 1 == h[i + 1][j]) {
-                    return LLONG_MAX;
-                }
+    auto check_row = [&](int i, int cur, int prev) {
+        for (int j = 0; j < n; ++j) {
+            if (h[i][j] + cur == h[i-1][j] + prev) {
+                return false;
             }
-
-            cost += a[i];
         }
 
-        return cost;
+        return true;
     };
 
-    for (auto& [l, r] : mer_seg_row) {
-        long long even = check_row_seg(true, l, r);
-        long long odd = check_row_seg(false, l, r);
+    array<vector<long long>, 2> dp_row{ vector<long long>(n, INF), vector<long long>(n, INF) };
+    dp_row[0][0] = 0LL;
+    dp_row[1][0] = a[0];
 
-        if (even == LLONG_MAX && odd == LLONG_MAX) {
-            cout << -1 << '\n';
-            return;
+    for (int i = 1; i < n; ++i) {
+        if (check_row(i, 0, 0)) {
+            dp_row[0][i] = min(dp_row[0][i-1], dp_row[0][i]);
         }
 
-        ans += min(even, odd);
+        if (check_row(i, 1, 0)) {
+            dp_row[1][i] = min(dp_row[0][i-1] + a[i], dp_row[1][i]);
+        }
+
+        if (check_row(i, 0, 1)) {
+            dp_row[0][i] = min(dp_row[1][i-1], dp_row[0][i]);
+        }
+
+        if (check_row(i, 1, 1)) {
+            dp_row[1][i] = min(dp_row[1][i-1] + a[i], dp_row[1][i]);
+        }
     }
 
-    cout << ans << '\n';
+    if (dp_row[0][n-1] == INF && dp_row[1][n-1] == INF) {
+        cout << -1 << '\n';
+        return;
+    }
+
+    cout << min(dp_row[0][n-1], dp_row[1][n-1]) + min(dp_col[0][n-1], dp_col[1][n-1]) << '\n';
 }
