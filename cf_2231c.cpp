07aca8f61922d3465bebedbedf0690b1ff4ccfa3
Kirill Katz <xnlyre@protonmail.com>
2026-05-26 17:12:29 +0300

    Solve

--- cf_2231c.cpp
+++ cf_2231c.cpp
@@ -20,5 +20,70 @@ int main() {
 }
 
 void solve() {
+    int n;
+    cin >> n;
 
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    unordered_map<int,long long> cnt;
+    for (int i = 0; i < n; ++i) {
+        int val = a[i];
+
+        unordered_set<int> seen;
+
+        while (true) {
+            seen.insert(val);
+
+            if (val % 2) {
+                seen.insert(val + 1);
+            }
+
+            if (val == 1) break;
+            val = (val + 1) / 2;
+        }
+
+        for (int x : seen) {
+            cnt[x]++;
+        }
+    }
+
+    auto check = [&](int max_val) {
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int val = a[i];
+            int local = 0;
+
+            while (true) {
+                if (val == max_val) {
+                    break;
+                }
+
+                if (val % 2) {
+                    local++;
+                    if (val + 1 == max_val) {
+                        break;
+                    }
+                }
+
+                if (val == 1) break;
+                local++;
+                val = (val + 1) / 2;
+            }
+
+            ans += local;
+        }
+        return ans;
+    };
+
+    int ans = INT_MAX;
+    for (auto [k, v] : cnt) {
+        if (v == n) {
+            ans = min(ans, check(k));
+        }
+    }
+
+    cout << ans << '\n';
 }
